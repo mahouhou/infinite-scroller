d83b41fe2c52faab1f179f55dc1067108bc805cd
mahouhou <freja_smith@hotmail.co.uk>
2023-05-19 17:25:48 +0100

    Set up GSAP and added simple tween that moves images from right to left across the screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-function App() {
+export default function App() {
+  // gsap.registerPlugin(ScrollTrigger);
+
+  const imgWidth = "400px"
+  const heroRef = useRef(null);
+
+  const BOX = '.hero'
+  console.log(heroRef.current)
+  // const BOXES = gsap.utils.toArray(BOX)
+  // const DURATION = 1
+  // const STAGGER = 0.1
+  // const OFFSET = 0
+  // const CYCLE_DURATION = STAGGER * BOXES.length
+  // const START_TIME = CYCLE_DURATION + DURATION * 0.5 + OFFSET
+
+  useEffect(() => {
+    gsap.timeline().fromTo(BOX, {
+      xPercent: 100
+    }, {
+      xPercent: -100,
+      repeat: -1,
+      stagger: 0.5,
+      duration: 1,
+      ease: 'none',
+    })
+  }, []);
+
+  const heroImages = heroArray.map((hero) => {
+    return (
+      <img
+        src={require("./assets/images/hero/" + hero + ".jpg")}
+        alt={hero}
+        key={hero}
+        className="hero"
+        style={{width: `${imgWidth}`}}
+        ref={heroRef}
+      />
+    );
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div id="gallery">
+          {heroImages}{heroImages}
+        </div>
+      </main>
     </div>
   );
 }
 
-export default App;
+const heroArray = ["maple0", "maple1", "maple2", "maple3", "maple4"];
